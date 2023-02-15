bbcf8da60362b79fcf04486f48299aa8c76eead6
Felipe Castillo <castillo.devsigner@icloud.com>
2023-02-15 11:46:07 -0500

    fix

--- js/cms.js
+++ js/cms.js
@@ -2,7 +2,6 @@ function render(container, message) {
   container.innerHTML = `<p>${message}</p>`;
 }
 
-<<<<<<< HEAD
 window.addEventListener("load", () => {
   (async function () {
     const app = document.getElementById("app");
@@ -18,18 +17,3 @@ window.addEventListener("load", () => {
     render(nav, `change this at the <a href="./pages/admin.html">admin</a>`);
   })();
 });
-=======
-(async function () {
-  const app = document.getElementById("app");
-
-  let about = await fetch("content/about.json");
-  let { message } = await about.json();
-
-  render(app, message);
-
-  const nav = document.createElement("nav");
-
-  app.appendChild(nav);
-  render(nav, `change this at the <a href="admin">admin</a>`);
-})();
->>>>>>> master
