296d6b66fde0108e4dd9e6cb0fdeac9a54b61e53
Felipe Castillo <castillo.devsigner@icloud.com>
2023-02-15 11:24:05 -0500

    feat(cms)

---
+++ js/cms.js
@@ -0,0 +1,19 @@
+function render(container, message) {
+  container.innerHTML = `<p>${message}</p>`;
+}
+
+window.addEventListener("load", () => {
+  (async function () {
+    const app = document.getElementById("app");
+
+    let about = await fetch("content/about.json");
+    let { message } = await about.json();
+
+    render(app, message);
+
+    const nav = document.createElement("nav");
+
+    app.appendChild(nav);
+    render(nav, `change this at the <a href="./pages/admin.html">admin</a>`);
+  })();
+});
