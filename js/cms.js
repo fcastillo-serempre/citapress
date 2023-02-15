a689cb832b47b9599de17bdaf3be3b7700de9582
Felipe Castillo <castillo.devsigner@icloud.com>
2023-02-15 12:39:41 -0500

    feat(book)

--- js/cms.js
+++ js/cms.js
@@ -4,16 +4,12 @@ function render(container, message) {
 
 window.addEventListener("load", () => {
   (async function () {
-    const app = document.getElementById("app");
-
-    let about = await fetch("content/about.json");
-    let { message } = await about.json();
-
-    render(app, message);
-
-    const nav = document.createElement("nav");
-
-    app.appendChild(nav);
-    render(nav, `change this at the <a href="./pages/admin.html">admin</a>`);
+    // const app = document.getElementById("app");
+    // let about = await fetch("content/about.json");
+    // let { message } = await about.json();
+    // render(app, message);
+    // const nav = document.createElement("nav");
+    // app.appendChild(nav);
+    // render(nav, `change this at the <a href="./pages/admin.html">admin</a>`);
   })();
 });
